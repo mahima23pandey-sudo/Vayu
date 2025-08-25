0787880d82f19f31f3b5a57d1900a7fd47350162
gauravmer0607 <mergaurav4@gmail.com>
2025-08-25 14:08:45 +0530

    home page script

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+
+function goAuth(){ 
+  alert('Login/Signup page placeholder'); 
+}
+function scrollToCards(){ document.getElementById('explore').scrollIntoView({
+  behavior:'smooth'}); 
+}
+function goExplore(){
+   window.location.href='explore.html'; 
+}
